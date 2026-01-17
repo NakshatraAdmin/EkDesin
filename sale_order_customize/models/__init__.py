8df337e3606e00253d44752efc55f04ba93c0e19
NakshatraAdmin <chandrakant@nakshatrainfo.com>
2026-01-17 14:36:04 +0530

    discount and term condition changes[ADD]

--- sale_order_customize/models/__init__.py
+++ sale_order_customize/models/__init__.py
@@ -4,3 +4,4 @@
 from . import res_partner
 from . import stock_move
 from . import sale_order
+from . import purchase_order_line
--- sale_order_customize/models/sale_order.py
+++ sale_order_customize/models/sale_order.py
@@ -90,3 +90,31 @@ class SaleOrder(models.Model):
                     continue
         
         return res
+
+from odoo import models, fields, api
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    discount_type = fields.Selection(
+        [
+            ("percent", "Percent"),
+            ("amount", "Amount"),
+        ],
+        default="percent",
+        string="Discount Type",
+    )
+
+    discount_amount = fields.Float(string="Discount Amount")
+
+    @api.onchange("discount_type", "discount_amount", "price_unit", "product_uom_qty")
+    def _onchange_discount_amount(self):
+        for line in self:
+            if line.discount_type == "amount":
+                total = line.price_unit * line.product_uom_qty
+                if total > 0:
+                    line.discount = (line.discount_amount / total) * 100
+                else:
+                    line.discount = 0.0
+            else:
+                line.discount_amount = 0.0
