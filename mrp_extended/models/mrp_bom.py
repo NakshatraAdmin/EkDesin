ed77614855c17e92dc37d9f0c714ec54f26f9109
Chirag Dudhait <chirag.dudhait143@gmail.com>
2026-04-10 17:07:54 +0530

    [update] 3 Decimal point in BOM componant field

--- mrp_extended/models/mrp_bom.py
+++ mrp_extended/models/mrp_bom.py
@@ -5,6 +5,13 @@ class MrpBomLine(models.Model):
     _inherit = "mrp.bom.line"
 
     note = fields.Html(string="Note")
+    secondary_product_uom_qty = fields.Float(
+        string="Secondary Quantity",
+        help="Secondary UoM Quantity",
+        compute="_compute_secondary_product_uom_qty",
+        store=True,
+        digits=(16, 3),
+    )
 
     @api.depends("product_qty", "product_id.sec_uom_ratio", 'length', 'width', 'height')
     def _compute_secondary_product_uom_qty(self):
