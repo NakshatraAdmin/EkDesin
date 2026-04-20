be55d553282a32f34680219da26dabe91ba9a8cc
Chirag Dudhait <chirag.dudhait143@gmail.com>
2026-04-20 10:59:29 +0530

    [fix] Default code search in sales order

--- bom_approval/models/sale_order.py
+++ bom_approval/models/sale_order.py
@@ -1,23 +1,19 @@
 from odoo import models, fields, _, api
+from odoo.api import depends
 
 
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
 
-    allowed_template_ids = fields.Many2many('product.template', compute='_compute_allowed_products')
-    allowed_product_ids = fields.Many2many('product.product', compute='_compute_allowed_products')
+    is_allowed_bom = fields.Boolean(compute="allowed_bom_product", store=True)
 
-    @api.depends('company_id')
-    def _compute_allowed_products(self):
-        for order in self:
-            all_templates = self.env['product.template'].search([])
-            approved_boms = self.env['mrp.bom'].search([('state', '=', 'approved')])
-            approved_templates = approved_boms.mapped('product_tmpl_id')
+    @api.depends('bom_ids', 'bom_ids.state')
+    def allowed_bom_product(self):
+        is_allowed_bom = False
+        for product in self:
+            if not product.bom_ids:
+                is_allowed_bom = True
+            if all (bom.state == 'approved' for bom in product.bom_ids):
+                is_allowed_bom = True
+            product.is_allowed_bom = is_allowed_bom
 
-            all_bom_templates = self.env['mrp.bom'].search([]).mapped('product_tmpl_id')
-            no_bom_templates = all_templates - all_bom_templates
-            final_templates = approved_templates | no_bom_templates
-            final_variants = final_templates.mapped('product_variant_ids')
-            for order in self:
-                order.allowed_template_ids = final_templates
-                order.allowed_product_ids = final_variants
